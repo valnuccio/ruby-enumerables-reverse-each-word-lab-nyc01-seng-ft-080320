f7723cb97945c5fd5f17849059b14454a9e87a05
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-29 13:15:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   new_array= []
-  sentence.split.each do |element|
+  sentence.split.collect do |element|
     new_array << element.reverse
     end
     new_array.join(' ')
