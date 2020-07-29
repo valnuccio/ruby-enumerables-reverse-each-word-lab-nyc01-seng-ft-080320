a180543cac95f01eb06b78d57f13b8e9d824b1c7
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-29 13:22:35 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
-def reverse_each_word(sentence)
-  new_array= []
-  sentence.reverse.split.each do |element|
-    new_array << element.reverse
-    end
-    new_array.join(' ')
+
+ 
+def reverse_each_word(string)
+  string.split.collect {|element|element.reverse}.join(' ')
   end
 
-reverse_each_word string
